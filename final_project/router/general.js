956989315831a602314313cd10f7614469ed8000
Ritz811 <rzainab811@gmail.com>
2024-09-14 11:32:09 -0400

    initial commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  for (var i = 0; i < books.length; i++){
+    if (books[i].author === author)
+    {
+        res.send(books[i])
+    }
+  }
 });
 
 // Get all books based on title
