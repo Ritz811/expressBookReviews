aaf012d397112d8028827529b2b0e7e477c61b07
Ritz811 <rzainab811@gmail.com>
2024-09-14 12:42:00 -0400

    initial commit

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,24 +17,20 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 regd_users.post("/login", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-    // Check if username or password is missing
     if (!username || !password) {
-        return res.status(404).json({ message: "Error logging in" });
+        return res.status(404).json({ message: 
+            "Error logging in, please try again." });
     }
-    // Authenticate user
     if (authenticatedUser(username, password)) {
-        // Generate JWT access token
         let accessToken = jwt.sign({
             data: password
         }, 'access', { expiresIn: 60 * 60 });
-        // Store access token and username in session
         req.session.authorization = {
             accessToken, username
         }
-        return res.status(200).send("User successfully logged in");
+        return res.status(200).send("User successfully logged in!");
     } else {
-        return res.status(208).json({ message: 
-            "Invalid Login. Check username and password" });
+        return res.status(208).json({ message: "Invalid Login, please check username and password and try again." });
     }
 });
 
